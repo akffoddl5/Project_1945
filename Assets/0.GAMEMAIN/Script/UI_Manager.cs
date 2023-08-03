c67ca1d9c626da7c9ce30052e6ddfb26d5dfd7c3
Dowoon123 <wmfahgo@naver.com>
2023-08-03 18:16:14 +0900

    부활찐완성
    1234

--- Assets/0.GAMEMAIN/Script/UI_Manager.cs
+++ Assets/0.GAMEMAIN/Script/UI_Manager.cs
@@ -7,285 +7,348 @@ using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
-	public static UI_Manager instance;
+    public static UI_Manager instance;
+    public bool b_isGameStart = false;
+    public GameObject obj_panel;
 
-	public GameObject obj_panel;
+    public Text gameStatus;
+    public GameObject obj_nextBtn;
+    public GameObject obj_reBtn;
 
-	public Text gameStatus;
-	public GameObject obj_nextBtn;
-	public GameObject obj_reBtn;
+    public string nextSceneName;
+    public string nowSceneName;
 
-	public string nextSceneName;
-	public string nowSceneName;
+    public GameObject DW_Player;
+    public GameObject JW_Player;
+    public GameObject YS_Player;
+    public GameObject JH_Player;
+    public GameObject SJ_Player;
+    public GameObject now_Player;
+    public GameObject now_Player_Instance;
 
-	public GameObject DW_Player;
-	public GameObject JW_Player;
-	public GameObject YS_Player;
-	public GameObject JH_Player;
-	public GameObject SJ_Player;
-	public GameObject now_Player;
-	public GameObject now_Player_Instance;
+    public Dictionary<Charactor, GameObject> prefab_dict = new Dictionary<Charactor, GameObject>();
+    public Dictionary<int, string> scene_dict = new Dictionary<int, string>();
 
-	public Dictionary<Charactor, GameObject> prefab_dict = new Dictionary<Charactor, GameObject>();
-	public Dictionary<int, string> scene_dict = new Dictionary<int, string>();
 
-	
 
-	public int current_stage = 1;
+    public int current_stage = 1;
 
-	Coroutine co_fadeIn;
-	Coroutine co_fadeOut;
+    Coroutine co_fadeIn;
+    Coroutine co_fadeOut;
 
-	private void Awake()
-	{
-		
-		if (instance == null)
-		{
-			instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-
-		prefab_dict.Add(Charactor.신준, SJ_Player);
-		prefab_dict.Add(Charactor.지원, JW_Player);
-		prefab_dict.Add(Charactor.정현, JH_Player);
-		prefab_dict.Add(Charactor.도운, DW_Player);
-		prefab_dict.Add(Charactor.용석, YS_Player);
-
-		scene_dict.Add(3, "Dowoon");
-		scene_dict.Add(2, "YONGSEOK");
-		scene_dict.Add(5, "Jiwon");
-		scene_dict.Add(1, "kjh_sceen1");
-		scene_dict.Add(4, "June_Scene");
-
-		current_stage = 1;
-	}
-
-	void Start()
+    private void Awake()
     {
-		_Init();
-		co_fadeIn = StartCoroutine(FadeIn());
-	}
 
-	void _Init()
-	{
-		obj_panel.GetComponent<Image>().color = Color.black; // 검정색으로 시작해서 페이드인 되어야 함
-		gameStatus.gameObject.SetActive(false);
-		obj_nextBtn.SetActive(false);
-		obj_reBtn.SetActive(false);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
-		SceneManager.sceneLoaded += test;
-	}
-	
-	
+        prefab_dict.Add(Charactor.신준, SJ_Player);
+        prefab_dict.Add(Charactor.지원, JW_Player);
+        prefab_dict.Add(Charactor.정현, JH_Player);
+        prefab_dict.Add(Charactor.도운, DW_Player);
+        prefab_dict.Add(Charactor.용석, YS_Player);
 
-	public void test(Scene arg0, LoadSceneMode arg1)
-	{
-		StartCoroutine(FadeIn());
-		StartCoroutine(Playerspawn());
-		//Debug.Log("스폰 시작, 페이드인 시작");
-	}
+        scene_dict.Add(5, "Dowoon");
+        scene_dict.Add(2, "YONGSEOK");
+        scene_dict.Add(1, "Jiwon");
+        scene_dict.Add(3, "kjh_sceen1");
+        scene_dict.Add(4, "June_Scene");
 
-	
+        current_stage = 1;
+    }
 
-	
+    void Start()
+    {
+        _Init();
+        co_fadeIn = StartCoroutine(FadeIn());
+        
 
-	public void CharacterSelect_FadeOut(Charactor a)
-	{
-		//StartCoroutine(FadeOut());
-		//Debug.Log(current_stage);
+    }
 
-		now_Player = prefab_dict[a];
-		nowSceneName = scene_dict[current_stage];
-		
-		StartCoroutine(LoadScene(1));
-		
-	}
+    void _Init()
+    {
+        obj_panel.GetComponent<Image>().color = Color.black; // 검정색으로 시작해서 페이드인 되어야 함
+        gameStatus.gameObject.SetActive(false);
+        obj_nextBtn.SetActive(false);
+        obj_reBtn.SetActive(false);
 
+        SceneManager.sceneLoaded += test;
+    }
 
-	public void GameClear_UI()
-	{
-		StartCoroutine(PlayerReverseSpawn());
-		
-		StartCoroutine(FadeOut(2f));
+    public void Revive()
+    {
+        StartCoroutine(PlayerRevive());
+    }
 
-		gameStatus.text = "Game Clear";
-		obj_nextBtn.GetComponent<Button>().interactable = true;
-		obj_reBtn.GetComponent<Button>().interactable = false;
-
-		gameStatus.gameObject.SetActive(true);
-		obj_nextBtn.gameObject.SetActive(true);
-		obj_reBtn.gameObject.SetActive(true);
-
-	}
-
-	
+    public void test(Scene arg0, LoadSceneMode arg1)
+    {
+        StartCoroutine(FadeIn());
+        StartCoroutine(Playerspawn());
+        //Debug.Log("스폰 시작, 페이드인 시작");
+    }
 
 
-	void GameOver_UI()
-	{
-		co_fadeOut = StartCoroutine(FadeOut());
+    public IEnumerator CheckPlayerDie()
+    {
+        while (true)
+        {
+            if (b_isGameStart)
+            {
+                if (now_Player_Instance == null)
+                {
+                    Debug.Log("부활 ui 오픈");
+                    GetComponent<UI_Revive>().SetSelectUI(true);
+                    break;
 
-		gameStatus.text = "Game Over";
-		obj_nextBtn.GetComponent<Button>().interactable = false;
-		obj_reBtn.GetComponent<Button>().interactable = true;
+                }
+            }
 
-		gameStatus.gameObject.SetActive(true);
-		obj_nextBtn.gameObject.SetActive(true);
-		obj_reBtn.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
 
-	}
 
-	public void NextBtn()
-	{
-		current_stage++;
+    public void CharacterSelect_FadeOut(Charactor a)
+    {
+        //StartCoroutine(FadeOut());
+        //Debug.Log(current_stage);
 
-		StartCoroutine(LoadScene(current_stage));
-	}
-	public void RestartBtn()
-	{
-		SceneManager.LoadScene(nowSceneName);
-	}
+        now_Player = prefab_dict[a];
+        nowSceneName = scene_dict[current_stage];
 
-	IEnumerator Playerspawn()
-	{
-		_Init();
-		if (GameObject.FindGameObjectWithTag("Player") != now_Player_Instance)
-		{
-			Debug.Log("플레이어 삭제됨");
-			Destroy(GameObject.FindGameObjectWithTag("Player"));
-		}
+        StartCoroutine(LoadScene(1));
 
-		if (now_Player_Instance == null)
-		{
-			now_Player_Instance = Instantiate(now_Player, new Vector3(0, -6, 0), Quaternion.identity);
-		}
+    }
 
-		//스폰 전처리
-		now_Player_Instance.transform.position = new Vector3(0, -6, 0);
 
-		//다형성으로 2D콜라이더 전부 Disable
+    public void GameClear_UI()
+    {
+        StartCoroutine(PlayerReverseSpawn());
+
+        StartCoroutine(FadeOut(2f));
+
+        gameStatus.text = "Game Clear";
+        obj_nextBtn.GetComponent<Button>().interactable = true;
+        obj_reBtn.GetComponent<Button>().interactable = false;
+
+        gameStatus.gameObject.SetActive(true);
+        obj_nextBtn.gameObject.SetActive(true);
+        obj_reBtn.gameObject.SetActive(true);
+
+    }
+
+
+
+
+    void GameOver_UI()
+    {
+        co_fadeOut = StartCoroutine(FadeOut());
+
+        gameStatus.text = "Game Over";
+        obj_nextBtn.GetComponent<Button>().interactable = false;
+        obj_reBtn.GetComponent<Button>().interactable = true;
+
+        gameStatus.gameObject.SetActive(true);
+        obj_nextBtn.gameObject.SetActive(true);
+        obj_reBtn.gameObject.SetActive(true);
+
+    }
+
+    public void NextBtn()
+    {
+        current_stage++;
+
+        StartCoroutine(LoadScene(current_stage));
+    }
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(nowSceneName);
+    }
+
+    public void PlayerDie()
+    {
+        GetComponent<UI_Revive>().SetSelectUI(true);
+    }
+
+    IEnumerator PlayerRevive()
+    {
+        if (now_Player_Instance != null)
+        {
+            Destroy(now_Player_Instance);
+        }
+
+
+        if (now_Player_Instance == null)
+        {
+            now_Player_Instance = Instantiate(now_Player, new Vector3(0, -6, 0), Quaternion.identity);
+        }
+
+        now_Player_Instance.transform.position = new Vector3(0, -6, 0);
+
+        //다형성으로 2D콜라이더 전부 Disable
         if (now_Player_Instance.GetComponent<Collider2D>() != null)
         {
-           now_Player_Instance.GetComponent<Collider2D>().enabled =  false;
+            now_Player_Instance.GetComponent<Collider2D>().enabled = false;
         }
 
 
         //스폰중..
         while (true)
-		{
-			//1초마다
-			yield return new WaitForSeconds(0.01f); //지연
-			now_Player_Instance.transform.Translate(0, 3 * Time.deltaTime, 0); //플레이어 생성 위치로부터 맵으로 끌고 오기.
+        {
+            //1초마다
+            yield return new WaitForSeconds(0.01f); //지연
+            now_Player_Instance.transform.Translate(0, 3 * Time.deltaTime, 0); //플레이어 생성 위치로부터 맵으로 끌고 오기.
 
-			if (now_Player_Instance.transform.position.y > -4)  break;
+            if (now_Player_Instance.transform.position.y > -4) break;
 
-		}
+        }
+    }
+    IEnumerator Playerspawn()
+    {
+        _Init();
+        if (GameObject.FindGameObjectWithTag("Player") != now_Player_Instance)
+        {
+            Debug.Log("플레이어 삭제됨");
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        }
 
-		//스폰후처리 (콜라이더 다시 킬지 말지)
-		//now_Player_Instance.GetComponent<Collider2D>().enabled = true;
-	}
+        if (now_Player_Instance == null)
+        {
+            now_Player_Instance = Instantiate(now_Player, new Vector3(0, -6, 0), Quaternion.identity);
+        }
 
-	IEnumerator PlayerReverseSpawn()
-	{
+        //스폰 전처리
+        now_Player_Instance.transform.position = new Vector3(0, -6, 0);
 
-		
-		//다형성으로 2D콜라이더 전부 Disable
-		if (now_Player_Instance.GetComponent<Collider2D>() != null)
-		{
-			now_Player_Instance.GetComponent<Collider2D>().enabled = false;
-		}
-
-
-		//리버스 스폰중..
-		while (true)
-		{
-			//1초마다
-			yield return new WaitForSeconds(0.01f); //지연
-			now_Player_Instance.transform.Translate(0, 9 * Time.deltaTime, 0); //플레이어 생성 위치로부터 맵으로 끌고 오기.
-
-			if (now_Player_Instance.transform.position.y > 6) break;
-
-		}
-
-		//yield return new WaitForSeconds(1);
-
-	}
+        //다형성으로 2D콜라이더 전부 Disable
+        if (now_Player_Instance.GetComponent<Collider2D>() != null)
+        {
+            now_Player_Instance.GetComponent<Collider2D>().enabled = false;
+        }
 
 
+        //스폰중..
+        while (true)
+        {
+            //1초마다
+            yield return new WaitForSeconds(0.01f); //지연
+            now_Player_Instance.transform.Translate(0, 3 * Time.deltaTime, 0); //플레이어 생성 위치로부터 맵으로 끌고 오기.
 
-	// 어두운 화면에서 게임 화면으로 전환됨
-	IEnumerator FadeIn()
-	{
-		Color tmp = new Color(0f, 0f, 0f, 0.05f);
-		while (obj_panel.GetComponent<Image>().color.a > 0)
-		{
-			yield return new WaitForSeconds(0.05f);
-			//Debug.Log(obj_panel.GetComponent<Image>().color.a);
-			obj_panel.GetComponent<Image>().color -= tmp;
-		}
-		yield break;
-	}
+            if (now_Player_Instance.transform.position.y > -4) break;
 
-	IEnumerator FadeOut()
-	{
-		Color tmp = new Color(0f, 0f, 0f, 0.05f);
-		while (obj_panel.GetComponent<Image>().color.a < 1)
-		{
-			yield return new WaitForSeconds(0.05f);
-			//Debug.Log(obj_panel.GetComponent<Image>().color.a);
-			obj_panel.GetComponent<Image>().color += tmp;
-		}
-		yield break;
-	}
+        }
 
-	IEnumerator FadeOut(float time_delay)
-	{
-		yield return new WaitForSeconds(time_delay);
-		Color tmp = new Color(0f, 0f, 0f, 0.05f);
-		while (obj_panel.GetComponent<Image>().color.a < 1)
-		{
-			yield return new WaitForSeconds(0.05f);
-			//Debug.Log(obj_panel.GetComponent<Image>().color.a);
-			obj_panel.GetComponent<Image>().color += tmp;
-		}
-		yield break;
-	}
+        b_isGameStart = true;
+        StartCoroutine(CheckPlayerDie());
+        //스폰후처리 (콜라이더 다시 킬지 말지)
+        now_Player_Instance.GetComponent<Collider2D>().enabled = true;
+    }
+
+    IEnumerator PlayerReverseSpawn()
+    {
+
+
+        //다형성으로 2D콜라이더 전부 Disable
+        if (now_Player_Instance.GetComponent<Collider2D>() != null)
+        {
+            now_Player_Instance.GetComponent<Collider2D>().enabled = false;
+        }
+
+
+        //리버스 스폰중..
+        while (true)
+        {
+            //1초마다
+            yield return new WaitForSeconds(0.01f); //지연
+            now_Player_Instance.transform.Translate(0, 9 * Time.deltaTime, 0); //플레이어 생성 위치로부터 맵으로 끌고 오기.
+
+            if (now_Player_Instance.transform.position.y > 6) break;
+
+        }
+
+        //yield return new WaitForSeconds(1);
+
+    }
 
 
 
-	IEnumerator LoadScene(int stage)
-	{
-		StartCoroutine(FadeOut());
-		yield return new WaitForSeconds(1);
+    // 어두운 화면에서 게임 화면으로 전환됨
+    IEnumerator FadeIn()
+    {
+        Color tmp = new Color(0f, 0f, 0f, 0.05f);
+        while (obj_panel.GetComponent<Image>().color.a > 0)
+        {
+            yield return new WaitForSeconds(0.05f);
+            //Debug.Log(obj_panel.GetComponent<Image>().color.a);
+            obj_panel.GetComponent<Image>().color -= tmp;
+        }
+        yield break;
+    }
 
-		AsyncOperation oper = SceneManager.LoadSceneAsync(scene_dict[stage]);
-		while (!oper.isDone)
-		{
-			yield return null;
-			//Debug.Log(oper.progress);
-		}
-		//StartCoroutine(FadeIn());
-		//Debug.Log("start cor");
-		//SceneManager.LoadScene(scene_dict[stage]);
+    IEnumerator FadeOut()
+    {
+        Color tmp = new Color(0f, 0f, 0f, 0.05f);
+        while (obj_panel.GetComponent<Image>().color.a < 1)
+        {
+            yield return new WaitForSeconds(0.05f);
+            //Debug.Log(obj_panel.GetComponent<Image>().color.a);
+            obj_panel.GetComponent<Image>().color += tmp;
+        }
+        yield break;
+    }
+
+    IEnumerator FadeOut(float time_delay)
+    {
+        yield return new WaitForSeconds(time_delay);
+        Color tmp = new Color(0f, 0f, 0f, 0.05f);
+        while (obj_panel.GetComponent<Image>().color.a < 1)
+        {
+            yield return new WaitForSeconds(0.05f);
+            //Debug.Log(obj_panel.GetComponent<Image>().color.a);
+            obj_panel.GetComponent<Image>().color += tmp;
+        }
+        yield break;
+    }
 
 
-	}
+
+    IEnumerator LoadScene(int stage)
+    {
+        StartCoroutine(FadeOut());
+        yield return new WaitForSeconds(1);
+
+        AsyncOperation oper = SceneManager.LoadSceneAsync(scene_dict[stage]);
+        while (!oper.isDone)
+        {
+            yield return null;
+            //Debug.Log(oper.progress);
+        }
+
+        //StartCoroutine(FadeIn());
+        //Debug.Log("start cor");
+        //SceneManager.LoadScene(scene_dict[stage]);
 
 
-	//private void OnLevelWasLoaded(int level)
-	//{
-	//	StopCoroutine(FadeOut());
-	//	GameObject a = GameObject.FindGameObjectWithTag("Player");
-	//	Destroy(a);
-	//	StartCoroutine(FadeIn());
+    }
 
-	//	now_Player_Instance = Instantiate(now_Player, new Vector3(0, -4, 0), Quaternion.identity);
-	//	DontDestroyOnLoad(now_Player_Instance);
-	//}
+
+    //private void OnLevelWasLoaded(int level)
+    //{
+    //	StopCoroutine(FadeOut());
+    //	GameObject a = GameObject.FindGameObjectWithTag("Player");
+    //	Destroy(a);
+    //	StartCoroutine(FadeIn());
+
+    //	now_Player_Instance = Instantiate(now_Player, new Vector3(0, -4, 0), Quaternion.identity);
+    //	DontDestroyOnLoad(now_Player_Instance);
+    //}
 
 
 }
---
+++ Assets/0.GAMEMAIN/Script/UI_Revive.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Revive : MonoBehaviour
+{
+    public GameObject[] SelectBtn;
+    public GameObject[] Character;
+    public GameObject SelectPanel;
+    public GameObject SelectArrow;
+
+
+    public Vector3 originArrowPos;
+    public bool _isSlectAble;
+
+    public int index = 0;
+
+    public int beforeIndex = 0;
+
+    private void Start()
+    {
+      
+       // StartCoroutine(MoveArrow());
+    }
+    public void SetSelectUI(bool b)
+    {
+  
+        SelectPanel.SetActive(b);
+
+        _isSlectAble = b;
+        SetArrowPos();
+
+        if (b)
+            StartCoroutine(MoveArrow());
+
+        if (!b)
+            StopAllCoroutines();
+    }
+
+
+    public void Update()
+    {
+
+
+
+        if (_isSlectAble)
+        {
+
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                index++;
+                
+                if(index >=5)
+                {
+                    index = 0;
+                }
+
+                SetArrowPos();
+             
+            }
+
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                index--;
+
+                if (index < 0)
+                {
+                  index =  SelectBtn.Length - 1;
+
+                }
+
+                SetArrowPos();
+            }
+
+            
+
+
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                GameObject instanceObject = null;
+                switch(index)
+                {
+                    case 0:
+                        instanceObject = UI_Manager.instance.SJ_Player;
+                        break;
+                    case 1:
+                        instanceObject = UI_Manager.instance.JW_Player;
+                        break;
+                    case 2:
+                        instanceObject = UI_Manager.instance.JH_Player;
+                        break;
+                    case 3:
+                        instanceObject = UI_Manager.instance.DW_Player;
+                        break;
+                    case 4:
+                        instanceObject = UI_Manager.instance.YS_Player;
+                        break;
+
+                }
+                UI_Manager.instance.now_Player = instanceObject;
+                UI_Manager.instance.Revive();
+                Debug.Log("ºÎÈ°µÊ");
+                
+                SetSelectUI(false);
+            }
+
+        }
+    }
+
+    public void SetArrowPos()
+    {
+        var pos = SelectArrow.transform.position;
+        pos.x = SelectBtn[index].transform.position.x;
+        SelectArrow.transform.position = pos;
+
+        SelectBtn[beforeIndex].GetComponent<Image>().color = Color.white;
+        SelectBtn[index].GetComponent<Image>().color = Color.green;
+   
+
+        beforeIndex = index;
+
+    }
+
+    IEnumerator MoveArrow()
+    {
+
+        bool b_up = true;
+
+        while (true)
+        {
+            if (b_up)
+            {
+                SelectArrow.transform.Translate(Vector3.up * 25.0f * Time.deltaTime);
+
+                if (SelectArrow.transform.localPosition.y >= -150f)
+                {
+                    b_up = false;
+                }
+
+
+            }
+            else
+            {
+                SelectArrow.transform.Translate(Vector3.up * -25.0f * Time.deltaTime);
+
+                if (SelectArrow.transform.localPosition.y <= -180f)
+                {
+                    b_up = true;
+                }
+
+            }
+
+            yield return new WaitForEndOfFrame();
+
+        }
+
+    }
+
+
+
+
+}
