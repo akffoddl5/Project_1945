9ac7a3420588117bf69709acd3e7ed630a7570a7
urshin <136461531+urshin@users.noreply.github.com>
2023-08-04 14:04:09 +0900

    수정된거 올립니다.

--- Assets/June/Script/June_BossMovement.cs
+++ Assets/June/Script/June_BossMovement.cs
@@ -112,13 +112,13 @@ public class June_BossMovement : MonoBehaviour
         ImpSound.Play();
         for (int i = 0; i < 22; i++)
         {
-            transform.GetChild(1).transform.GetChild(1).transform.position += new Vector3(50, 0, 0);
+            transform.GetChild(1).transform.GetChild(1).transform.localPosition += new Vector3(50, 0, 0);
             yield return new WaitForSeconds(0f);
 
         }
 
         yield return new WaitForSeconds(1f);
-        transform.GetChild(1).transform.GetChild(1).transform.position = new Vector3(-1100, 638, 0);
+        transform.GetChild(1).transform.GetChild(1).transform.localPosition = new Vector3(-1100, 638, 0);
         isbossTalk5 = false;
     }
     IEnumerator showBossTxt3()
@@ -128,12 +128,12 @@ public class June_BossMovement : MonoBehaviour
         ImpSound.Play();
         for (int i = 0; i < 22; i++)
         {
-            transform.GetChild(1).transform.GetChild(0).transform.position += new Vector3(50, 0, 0);
+            transform.GetChild(1).transform.GetChild(0).transform.localPosition += new Vector3(50, 0, 0);
             yield return new WaitForSeconds(0.01f);
 
         }
         yield return new WaitForSeconds(1f);
-        transform.GetChild(1).transform.GetChild(0).transform.position = new Vector3(-1100, 638, 0);
+        transform.GetChild(1).transform.GetChild(0).transform.localPosition = new Vector3(-1100, 638, 0);
         isbossTalk3 = false;
     }
     void Stop()
